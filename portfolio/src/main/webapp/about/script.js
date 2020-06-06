349a9a2f1ef0a6b707dc8ad5dee842db12317975
Daniel Torres <daniel.torres10@upr.edu>
2020-06-06 16:58:05 -0400

    Finish step 2: Fetch

--- portfolio/src/main/webapp/about/script.js
+++ portfolio/src/main/webapp/about/script.js
@@ -12,17 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// /**
+//  * Adds a random greeting to the page.
+//  */
+// function addRandomGreeting() {
+//   const greetings =
+//       ['Born and raised in Puerto Rico!', 'Father of four dogs', 'Favorite Movie: Inception', 'Can play percussion.'];
+
+//   // Pick a random greeting.
+//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+//   // Add it to the page.
+//   const greetingContainer = document.getElementById('greeting-container');
+//   greetingContainer.innerText = greeting;
+// }
+
 /**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Born and raised in Puerto Rico!', 'Father of four dogs', 'Favorite Movie: Inception', 'Can play percussion.'];
+* week-2 Step 2:
+*/
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+/* Fetch promise */
+function getHello(){
+  console.log('Fetching message...')
+  const promise = fetch('/data');
+  promise.then(handlePromise);
+} 
+/* Get promise content */
+function handlePromise(response) {
+  console.log('Handling the response.');
+  const textPromise = response.text();
+  textPromise.then(addQuoteToDom);
+}
+/* Add the content to html */
+function addQuoteToDom(quote) {
+  console.log('Adding quote to dom: ' + quote);
+  const quoteContainer = document.getElementById('server-container');
+  quoteContainer.innerText = quote;
+}
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+/* Other short method */
+function getHelloUsingArrowFunctions() {
+  fetch('/data').then(response => response.text()).then((quote) => {
+    document.getElementById('server-container').innerText = quote;
+  });
+}
+/* Other short method (better?) */
+async function getHelloUsingAsyncAwait() {
+  console.log('Fetching message...')
+  const response = await fetch('/data');
+  console.log('Handling the response.');
+  const quote = await response.text();
+  console.log('Adding quote to dom: ' + quote);
+  document.getElementById('server-container').innerText = quote;
 }
